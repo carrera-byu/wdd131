6f0749db0cc7508c48e57f4b0b07c5ab7419986a
carrera-byu <vcarrera@byupathway.edu>
2026-09-17 13:40:11 -0300

    S03 - Tarefa: Página do País

--- scripts/local.js
+++ scripts/local.js
@@ -5,10 +5,9 @@ const temp = parseFloat(document.getElementById('temp').textContent);
 const wind = parseFloat(document.getElementById('wind').textContent);
 let chill = "N/A";
 
-
 if (temp <= 10 && wind > 4.8) {
     chill = (13.12 + 0.6215 * temp - 11.37 * Math.pow(wind, 0.16) + 0.3965 * temp * Math.pow(wind, 0.16)).toFixed(1) + " °C";
 } else {
-    chill = temp + " °C";
+    chill = "N/A";
 }
 document.getElementById('windchill').textContent = chill;
